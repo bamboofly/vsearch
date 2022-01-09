455e268fbacbaa2ce92d7d702c4ac33b5d86c96f
bamboofly <964827222@qq.com>
2022-01-09 16:08:51 +0800

    添加注释

--- vSearch/src/main/java/com/uan/vsearch/IFastSearch.java
+++ vSearch/src/main/java/com/uan/vsearch/IFastSearch.java
@@ -2,6 +2,17 @@ package com.uan.vsearch;
 
 import java.util.List;
 
+/**
+ * 快速搜索接口定义
+ */
 interface IFastSearch {
+    /**
+     * 快速搜索接口，传入搜索key和搜索模糊距离，返回搜索结果。
+     * 被搜索字符串需要在初始化时传入{@link com.uan.vsearch.MdSearch.Builder#build(List)}
+     *
+     * @param voice 搜索关键字符串
+     * @param dis   搜索模糊距离
+     * @return 搜索结果集合
+     */
     List<SearchResult> search(String voice, float dis);
 }
--- vSearch/src/main/java/com/uan/vsearch/IMdSearch.java
+++ vSearch/src/main/java/com/uan/vsearch/IMdSearch.java
@@ -2,9 +2,28 @@ package com.uan.vsearch;
 
 import java.util.List;
 
+/**
+ * 普通模糊搜索接口，通过{@link MdSearch.Builder#build()}获得接口实例
+ */
 public interface IMdSearch {
 
+    /**
+     * 通过传入被搜索的字符串集合和搜索关键字，返回搜索结果。
+     * 搜索模糊距离为{@link MdSearch#NEAR_SEARCH_DEPTH_DEFAULT}
+     *
+     * @param list 被搜索字符串集合
+     * @param key  搜索关键字符串
+     * @return 搜索结果。按相似得分降序
+     */
     List<SearchResult> search(List<String> list, String key);
 
+    /**
+     * 通过传入被搜索的字符串集合、搜索关键字符串和搜索模糊距离，返回搜索结果。
+     *
+     * @param list      被搜索字符串集合
+     * @param key       搜索关键字符串
+     * @param nearDepth 搜索模糊距离。范围0～1
+     * @return 搜索结果。按相似得分降序
+     */
     List<SearchResult> search(List<String> list, String key, float nearDepth);
 }
